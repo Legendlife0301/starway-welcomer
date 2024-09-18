ea93033960e8da96273ddb9a39b5dd9e3918b156
Legendlife0301 <legendlife0301@gmail.com>
2024-09-18 20:00:43 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        TOKEN: process.env.token1,
+        PREFIX: '!',
+        VOICE_CHANNEL_ID: "1226237042006953999"
+    },
+    {
+        TOKEN: process.env.token2,
+        PREFIX: '!',
+        VOICE_CHANNEL_ID: "1226237042006954000"
+    },
+    {
+        TOKEN: process.env..token3,
+        PREFIX: '!',
+        VOICE_CHANNEL_ID: "1226237042006954001"
+    },
+    {
+        TOKEN: process.env.token4,
+        PREFIX: '!',
+        VOICE_CHANNEL_ID: "1279088368483045459"
+    },
+    {
+        TOKEN: process.env.token5,
+        PREFIX: '!',
+        VOICE_CHANNEL_ID: "1279088435512082525"
+    },
+];
